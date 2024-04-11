55b3cbc01264356e0f7a31a9d51f38296bce825e
Mykola T <anteycu@gmail.com>
2024-04-11 17:40:04 +0300

    refactored promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,6 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import PromisePlugin from './promise-plugin';
+
 const formRef = document.querySelector('.form');
-formRef.addEventListener('submit', submitHandler);
+const promises = new PromisePlugin();
 
-function submitHandler(e) {
-  e.preventDefault();
-  const { delay, step, amount } = e.currentTarget.elements;
-
-  promiseCaller(Number(delay.value), Number(step.value), amount.value);
-}
-
-function promiseCaller(delay, step, amount) {
-  let counterOfDelay = delay;
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, counterOfDelay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-          clickToClose: true,
-        });
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-          clickToClose: true,
-        });
-      });
-
-    counterOfDelay += step;
-  }
-}
-
-function createPromise(position, delay) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        // Fulfill
-        res({ position, delay });
-      } else {
-        // Reject
-        rej({ position, delay });
-      }
-    }, delay);
-  });
-}
+formRef.addEventListener('submit', promises.submit.bind(promises));
